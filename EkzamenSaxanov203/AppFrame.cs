9e8ac3622b84947199f94b4e2fca3c526dcc0ab4
gruppa203 <gruppa203git@mail.ru>
2023-12-08 12:03:31 +0300

    Initial commit

---
+++ EkzamenSaxanov203/AppFrame.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EkzamenSaxanov203.Pages;
+
+namespace EkzamenSaxanov203
+{
+    class AppFrame
+    {
+        public static AppFrame MainFrame { get; set; }
+    }
+}
